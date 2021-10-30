ffd3ab5184405afb4475b2c7d7b2694c9ecdf1fb
eliaszanotti <zanotti.elias@gmail.com>
2021-10-30 20:08:38 +0200

    ajout de get_cover()

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def search(search_query) :
     search = deezer_request(request_str)
     return search["data"]
 
+def get_cover(id_album) : 
+    request_str = '/album/' + str(id_album)
+    album = deezer_request(request_str)
+    return album["cover_xl"]
+
 def search_results(search_query) : 
     new_query = ""
     for char in search_query : 
@@ -24,17 +29,17 @@ def search_results(search_query) :
     result = search(new_query)
     list_albums = []
     for i in range(len(result)) : 
-        id_album = result[i]["album"]["id"]
         artist = result[i]["artist"]["name"]
         album = result[i]["album"]["title"]
-        final = str(id_album) + " : " + artist + " - " + album
+        cover_link = get_cover(result[i]["album"]["id"])
+        final = str(artist) + " - " + str(album) + " | " + str(cover_link)
         if final not in list_albums : 
             list_albums.append(final)
     for album in list_albums : 
         print(album)
     return 
 
-search_results('feu ')
+search_results('feu')
 
 
 
